20e048c2076b9920fb992d0d3ea49f5bdad14f3b
seanp-p <153692486+seanp-p@users.noreply.github.com>
2025-07-11 14:30:36 +0800

    Update streamlit_app.py
    Added pull requests from smoothie froot

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(name_on_order+'\'s Smoothie is ordered!', icon="✅")
+
+#New section to display smoothiesfroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
